04faa2ef820c01dee1a4bc37aac6df38e1483fd9
MAC <mac@MACs-MacBook-Pro.local>
2022-04-03 03:15:11 +0100

    Connect to db

--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,33 @@
 const express = require('express');
-const mysql = require('mysql');
-
+require('dotenv').config();
+const sql = require('mssql')
 const app = express();
-const port = 3001;
-const table ='users';
-
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PWD,
-  database: process.env.MYSQL_DB,
-});
+const port = 5500;
+const table = 'customers'
 
 app.listen(port, () => {
   console.log(`App server now listening to port ${port}`);
 });
 
-app.get('/api/users', (req, res) => {
-  pool.query(`select * from ${table}`, (err, rows) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(rows);
-    }
+const sqlConfig = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PWD,
+  database: process.env.MYSQL_DB,
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
+app.get('/api/customers', (req, res) => {
+  sql.connect(sqlConfig)
+  sql.query(`select * from ${table}`, (err, rows) => {
+    res.send(rows);
   });
 });
